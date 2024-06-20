da0207e0617384f78a75e9f5fd71407a75e3af14
anupamk98 <142046010+anupamk98@users.noreply.github.com>
2024-06-20 21:50:28 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="logo font-bold text-2xl text-green-800 p-1">&lt;<span className='text-green-800'>Pass</span>
       <span className='text-white'>Saver</span>/&gt;</div>
         <div className='flex align-center cursor-pointer hover:bg-green-700 p-1 rounded-full '>
-        <span><img src="github_mark.svg" alt="" className='invert w-10'/></span>
+        <span><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" className='invert w-10'/></span>
         <span className='font-bold text-xl text-white px-3 py-1 '>Github</span>
         </div>
     </nav>
